c2a361aac1179d9b5634c174195ff827d5ec7ff3
khadaroo <khadaro2024@gmail.com>
2025-09-08 00:27:49 +0300

    add basic routes and page files with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages.jsx/Login";
+import Signup from "./pages.jsx/Signup";
+import ForgotPassword from "./pages.jsx/ForgotPassword";
+import ResetPassword from "./pages.jsx/ResetPassword";
+import ProtectedRoutes from "./Components/ProtectedRoutes";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route element={<ProtectedRoutes />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Components/ProtectedRoutes.jsx
@@ -0,0 +1,3 @@
+export default function ProtectedRoutes() {
+  return <div>ProtectedRoutes</div>;
+}
